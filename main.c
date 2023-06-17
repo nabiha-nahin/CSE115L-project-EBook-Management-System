a8c75f199b8a4843a467bdac4bc3734da473f960
Nabiha-Nahin <131042913+nabiha-nahin@users.noreply.github.com>
2023-06-18 00:48:02 +0600

    Update main.c

--- main.c
+++ main.c
@@ -2,22 +2,3 @@
 #include <stdlib.h>
 
 
-
- int main( void )
- {
-
-  int x;
-  int y;
-  int z;
-  printf("Input the value of x: ");
-  scanf("%d", &x);
-  printf("Input the value of y: ");
-  scanf("%d", &y);
-  printf("z: ");
-  scanf("%d", &z);
-
-  printf("The total is %d\n", x*y*z);
-  printf("The total is %d", x+y-z);
-
-  return 0;
-}
