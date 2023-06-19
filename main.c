3edad41c333fbb34ed655b5f1bd3dbfc40e2879f
Nabiha-Nahin <131042913+nabiha-nahin@users.noreply.github.com>
2023-06-19 15:42:11 +0600

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,49 @@
 #include <stdio.h>
-#include <stdlib.h>
-int main(void){
+#include "splash.h"
+#include "login.h"
+#include "menu.h"
 
-
-
-
+int main(void) {
     
-return 0;
+    displaySplashScreen();
 
+ 
+    if (loginUser()) {
+     
+        printf("Login successful.\n");
+
+        int choice;
+        do {
+            
+            displayMenu();
+
+            // Asking for the choice of user
+            scanf("%d", &choice);
+            getchar(); //For a newline
+
+            //Operating with the choice of user
+            switch (choice) {
+                case 1:
+                    addBook();
+                    break;
+                case 2:
+                    searchBook();
+                    break;
+                case 3:
+                    displayAllBooks();
+                    break;
+                case 4:
+                    quitProgram();
+                    break;
+                default:
+                    printf("Invalid choice. Please try again.\n");
+                    break;
+            }
+        } while (choice != 4); // Repeat until the user chooses to quit
+    } else {
+        
+        printf("Login failed.\n");
+    }
+
+    return 0;
 }
