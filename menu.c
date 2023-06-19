40bdc485b90f20076d86e5877f3d30dc344b16c8
Nabiha-Nahin <131042913+nabiha-nahin@users.noreply.github.com>
2023-06-19 15:38:48 +0600

    Create menu.c

---
+++ menu.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "menu.h"
+
+void displayMenu(){
+
+    printf("\n");
+    printf("1. Add a book\n");
+    printf("2. Search for a book\n");
+    printf("3. Display all books\n");
+    printf("4. Quit\n");
+    printf("Enter your choice: ");
+}
+
+void addBook() {
+
+    printf("Adding a book.\n");
+}
+
+void searchBook() {
+
+    printf("Searching for a book.\n");
+}
+
+void displayAllBooks() {
+  
+    printf("Displaying all books.\n");
+}
+
+void quitProgram() {
+  
+    printf("Thank you for using the E-Book management system.\n");
+}
