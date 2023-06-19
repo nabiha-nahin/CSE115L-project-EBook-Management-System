6b0d132359ca0d27535b826c60392e7720dfc336
Nabiha-Nahin <131042913+nabiha-nahin@users.noreply.github.com>
2023-06-19 15:35:42 +0600

    Update menu.h

--- menu.h
+++ menu.h
@@ -1,10 +1,21 @@
+
 #define MENU_H
 
+// Display the main menu
 void displayMenu();
+
+//Adding a book
 void addBook();
-void searchForBooks();
+
+//Searching for a book
+void searchBook();
+
+//Displaying all the books
 void displayAllBooks();
+
+//Quitting the program
 void quitProgram();
 
-#endif
+
+
 
