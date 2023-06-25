69022e34af960522cd29a1ec779baa8a89fd09fb
Nabiha-Nahin <131042913+nabiha-nahin@users.noreply.github.com>
2023-06-25 09:58:12 +0600

    Update login.c

--- login.c
+++ login.c
@@ -2,9 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 int isRegisteredUser(const char* username) {
     FILE* usersFile = fopen("login.txt", "r");
+
+    if (usersFile == NULL) {
+        printf("Failed to open the users file.\n");
+        return 0;
+    }
 
     char storedUsername[100];
     while (fscanf(usersFile, "%99[^,\n]\n", storedUsername) == 1) {
@@ -21,30 +25,25 @@ int isRegisteredUser(const char* username) {
 int registerUser() {
     char username[100];
     char password[100];
-    char nsu_id[100];
 
-    printf("Enter a username: ");
+    printf("Enter your username: ");
     scanf("%99s", username);
 
     if (isRegisteredUser(username)) {
-        printf("Username already exists. Please choose a different username.\n");
+        printf("Username already exists.\n");
         return 0;
     }
-
-
-      printf("Enter nsu_id: ");
-    scanf("%99s", nsu_id);
 
     printf("Enter a password: ");
     scanf("%99s", password);
 
-    FILE* usersFile = fopen("users.txt", "a");
+    FILE* usersFile = fopen("login.txt", "a");
     if (usersFile == NULL) {
         printf("Failed to open the users file.\n");
         return 0;
     }
 
-    fprintf(usersFile, "%s,%s,%s\n", username, nsu_id, password);
+    fprintf(usersFile, "%s,%s\n", username, password);
     fclose(usersFile);
 
     printf("Registration successful.\n");
@@ -52,45 +51,59 @@ int registerUser() {
 }
 
 int loginUser() {
+    int i;
     char username[100];
-    char nsu_id[100];
     char password[100];
 
     printf("Enter your username: ");
     scanf("%99s", username);
 
-    if (!isRegisteredUser(username)) {
-        printf("Username not found.\n");
+    if (username[0] == '\0') {
+        printf("Username cannot be empty.\n");
         return 0;
     }
 
-    printf("Enter your password: ");
-    scanf("%99s", nsu_id);
+    // Read the usernames from the file
+    FILE* usersFile = fopen("login.txt", "r");
+    char storedUsernames[100][100];
+    int numStoredUsernames = 0;
+    while (fscanf(usersFile, "%99[^,\n]\n", storedUsernames[numStoredUsernames]) == 1) {
+        numStoredUsernames++;
+    }
+    fclose(usersFile);
+
 
     printf("Enter your password: ");
     scanf("%99s", password);
 
-    FILE* usersFile = fopen("users.txt", "r");
-
-    char storedUsername[100];
-    char storedNsu_ID[100];
-    char storedPassword[100];
-    int loginSuccessful = 0;
-
-    while (fscanf(usersFile, "%99[^,\n],%99[^,\n],%99[^\n]\n", storedUsername,storedNsu_ID, storedPassword) == 2) {
-        if (strcmp(storedUsername, username) == 0 && strcmp(storedNsu_ID, nsu_id) && strcmp(storedPassword, password) == 0) {
-            loginSuccessful = 1; // User logged in
+    // Check if the username is in the array of stored usernames
+    for (int i = 0; i < numStoredUsernames; i++) {
+        if (strcmp(username, storedUsernames[i]) == 0) {
+            // Username found
             break;
         }
     }
 
-    fclose(usersFile);
-
-    if (loginSuccessful) {
-        printf("Login successful.\n");
-        return 1;
-    } else {
-        printf("Incorrect password.\n");
+    // Check if the password is correct
+    if (i == numStoredUsernames) {
+        printf("Username not found.\n");
         return 0;
+    } else {
+        FILE* usersFile = fopen("login.txt", "r");
+
+        char storedUsername[100];
+        char storedPassword[100];
+        int loginSuccessful = 0;
+
+        while (fscanf(usersFile, "%99[^,\n],%99[^\n]\n", storedUsername, storedPassword) == 2) {
+            if (strcmp(storedUsername, username) == 0 && strcmp(storedPassword, password) == 0) {
+                loginSuccessful = 1; // User logged in
+                break;
+            }
+        }
+
+        fclose(usersFile);
+
+       
     }
 }
